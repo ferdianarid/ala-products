3581acaab33fa8b2b659d8648fdddfc83f13968d
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-22 12:46:46 +0700

    update users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Users } from "./entity/users.entity"
 
 @Module({
-       imports: [TypeOrmModule.forRoot({
-              type: 'mysql',
-              host: 'localhost',
-              port: 3306,
-              username: 'root',
-              password: '',
-              database: 'alaproduct',
-              entities: [],
-              synchronize: true,
-       })],
+       imports: [TypeOrmModule.forFeature([Users])],
        controllers: [UsersController],
        providers: [UsersService]
 })
