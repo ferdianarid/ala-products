02a67e1a52ff13cfd7d6ea1423de38914e80df51
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-22 15:34:30 +0700

    update modules

---
+++ src/modules/users.module.ts
@@ -0,0 +1,23 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from '../common/controllers/users.controller';
+import { UsersService } from '../common/services/users.service';
+import { Users } from "../entity/users.entity"
+import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+
+@Module({
+       imports: [TypeOrmModule.forFeature([Users])],
+       controllers: [UsersController],
+       providers: [UsersService]
+})
+
+export class UsersModule implements NestModule {
+       configure(consumer: MiddlewareConsumer) {
+              consumer
+                     .apply(LoggerMiddleware)
+                     .forRoutes({
+                            path: "users",
+                            method: RequestMethod.GET
+                     });
+       }
+}
